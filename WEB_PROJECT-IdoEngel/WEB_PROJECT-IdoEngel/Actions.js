8b412ee98cc4b358369a774b68589026a0f01b0b
Ido Engel <idoengel0707@gmail.com>
2024-11-17 19:52:54 +0200

    Added AboutUs.html & Actions.js fully working. Added conformation for loging out (in Actions.js

---
+++ WEB_PROJECT-IdoEngel/WEB_PROJECT-IdoEngel/Actions.js
@@ -0,0 +1,48 @@
+﻿// ID getters
+const LogOut = document.getElementById('LogOut'); // log out conversation
+const AboutUs = document.getElementsByClassName("accordion"); // unfold about us sections
+
+
+//Action listeners
+LogOut.addEventListener('click', function (event) { // log out conversation
+    askToLogOut(event);
+});
+
+for (let i = 0; i < AboutUs.length; i++) { // LOOP REQURED
+    AboutUs[i].addEventListener("click", function () { // for each tag..
+        unfoldAboutUs(this);
+    });
+}
+
+
+//Functions
+
+/**
+ * ask the user if he wants to log out (confirm the loging out from the account)
+ * @param {any} event - click accured
+ */
+function askToLogOut(event) {
+    console.log("Hellll");
+    const msg = "Are you sure you want to log out?";
+    const URL = "UserNavigation.html";
+
+    event.preventDefault();
+
+    if (confirm(msg)) {
+        window.location.href = URL;
+    }
+}
+
+/**
+ * unfold the about us section
+ * @param {any} th - this (curr item from the loop)
+ */
+function unfoldAboutUs(th) {
+    th.classList.toggle("active");
+    var panel = th.nextElementSibling;
+    if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+    } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+}
